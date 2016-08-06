3f8b127de1ffa35646e84152ab77bd3bf625d649
Eddie Appell <webdev@appell.com>
2016-08-06 09:48:24 -0700

    Add files via upload

---
+++ common/userAccount.js
@@ -0,0 +1,13 @@
+(function(){
+    "use strict";
+
+    angular
+        .module("common.services")
+        .factory("userAccount", ["$resource", "appSettings", userAccount]);
+
+    function userAccount($resource, appSettings) {
+        return $resource(appSettings.serverPath + "/api/Account/Register", null, {
+            'registerUser' : { method: 'POST' },
+        });
+    }
+})();
